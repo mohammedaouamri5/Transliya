ca08a90ea0ee267b2ad61d06b53d99256ace86d3
mohammedaouamri5 <mohammedaouamri5@gmail.com>
2024-06-09 21:05:59 +0000

    add BDD

--- back/Transliya/BDD/apps.py
+++ back/Transliya/BDD/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DbConfig(AppConfig):
+class BddConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'db'
+    name = 'BDD'
--- back/Transliya/Transliya/settings.py
+++ back/Transliya/Transliya/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    
+    
+    'BDD',
 ]
 
 MIDDLEWARE = [
@@ -73,14 +77,23 @@ WSGI_APPLICATION = 'Transliya.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'Transliya',
+        'USER': 'postgres',
+        'PASSWORD': 'MOHAMMED.ADMIN',
+        'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
