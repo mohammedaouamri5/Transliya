37547610de20ef9c10f59bb84bae3491e57268d9
mohammedaouamri5 <mohammedaouamri5@gmail.com>
2024-06-14 15:23:01 +0000

    fix SGBD

--- back/Transliya/Transliya/settings.py
+++ back/Transliya/Transliya/settings.py
@@ -87,26 +87,29 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Transliya.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+    # Database
+    # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'transliya',
-        'USER': 'postgres',
-        'PASSWORD': 'MOHAMMED.ADMIN',
-        'HOST': 'localhost',
-        # 'PORT': '5432',
+if DEBUG : 
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else : 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'transliya',
+            'USER': 'postgres',
+            'PASSWORD': 'MOHAMMED.ADMIN',
+            'HOST': 'localhost',
+            # 'PORT': '5432',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
